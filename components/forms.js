620085e550fd39f674462509dca09f9758722259
dave-torr <djjabbua@gmail.com>
2021-12-15 12:58:47 -0500

    added picture picker, logo switcher, and highlight adder remover

--- components/forms.js
+++ components/forms.js
@@ -1,6 +1,6 @@
-
 import React from "react"
 import { useState, useEffect } from "react"
+import Image from "next/image"
 
 import galapagosSunset from "../public/assets/images/tourCovers/galapagosSunset.jpg"
 import machuPicchu from "../public/assets/images/tourCovers/peruMachuPicchu.jpg"
@@ -15,32 +15,106 @@ import quitoSunrise from "../public/assets/images/tourCovers/quitoSunrise.jpg"
 import ruminahuiMountain from "../public/assets/images/tourCovers/ruminahuiMountain.jpg"
 import chimborazoVicunas from "../public/assets/images/tourCovers/chimborazoVicunas.jpg"
 import cotopaxiAerial from "../public/assets/images/tourCovers/cotopaxiAerial.jpg"
+//////////////////////////////////////////////////////////
 
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 import styles from "../styles/components/forms.module.css"
 
+
+export function LogoSwitcher(props){
+    const handleChange=()=>{
+        if(props.aTour.ecoAndesLogo){
+            props.tourEditor({...props.aTour, "ecoAndesLogo": false}) 
+        } else {
+            props.tourEditor({...props.aTour, "ecoAndesLogo": true}) 
+        }
+    }
+    return(<>
+        <div style={{display: "flex", alignItems: "center", fontSize: "1.3em"}}>
+        <Switch 
+            checked={props.aTour.ecoAndesLogo}
+            onChange={handleChange}
+        /> EcoAndes Travel Logo</div>
+    </>)
+}
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 let imageThumbnailArr=[
-    galapagosSunset,
-    machuPicchu,
-    cotopaxiClimb,
-    galapagosCruising,
-    galapagosExploration,
-    galapagosBeach,
-    boliviaMountains,
-    hikingsouthAmerica,
-    sunriseMountains,
-    quitoSunrise,
-    ruminahuiMountain,
-    chimborazoVicunas,
-    cotopaxiAerial
+    {"src": galapagosSunset, "alt": "Galapagos Sunset"},
+    {"src": cotopaxiClimb, "alt": "Cotopaxi Ecuador"},
+    {"src": galapagosCruising, "alt": "Galapagos Cruising"},
+    {"src": galapagosExploration, "alt": "Galapagos Exploration"},
+    {"src": galapagosBeach, "alt": "Galapagos Beach"},
+    {"src": machuPicchu, "alt": "Machu Picchu"},
+    {"src": boliviaMountains, "alt": "Bolivia Mountains"},
+    {"src": hikingsouthAmerica, "alt": "Hiking South America"},
+    {"src": sunriseMountains, "alt": "Mountains Sunrise"},
+    {"src": quitoSunrise, "alt": "Quito Sunrise"},
+    {"src": ruminahuiMountain, "alt": "Ruminahui Hike"},
+    {"src": chimborazoVicunas, "alt": "Chimborazo Vicunas"},
+    {"src": cotopaxiAerial, "alt": "Cotopaxi Aerial"},
 ]
+export function ItineraryImagePicker(props){
+    const [imageCap, setImageCap]=useState("Please Pick an image!")
 
-
-
+    let thumbNails = imageThumbnailArr.map((elem, i)=><React.Fragment key={i}>
+        <div className={styles.imageThumbnail} 
+            onMouseEnter={()=>{setImageCap(elem.alt)}} 
+            onMouseLeave={()=>setImageCap("Please Pick an image!")}
+            onClick={()=>props.tourEditor({...props.aTour, "tourCover": elem})}
+            >
+        <Image 
+            src={elem.src}
+            alt={elem.alt}
+            layout="responsive"
+        /></div>
+    </React.Fragment>)
+    return(
+        <>
+            <div className={styles.imageThumbnailDisp}> 
+            {thumbNails} 
+            </div>
+            <div> {imageCap}</div>
+        </>
+    )
+}
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+import AddBoxIcon from '@mui/icons-material/AddBox';
+export function HighlightAdder(props){
+    const [aHighlight, setAHighlight]=useState('')
+    return(
+        <div className={styles.highlightInputCont}>
+            <input
+                placeholder="Add a Highlight"
+                onChange={(e)=>{
+                    setAHighlight(e.target.value)
+                }}
+                style={{width: "250px"}}
+                value={aHighlight}
+            /> &nbsp;
+            {aHighlight===''?<>
+                <AddBoxIcon />
+            </>:<>
+                <AddBoxIcon onClick={()=>{
+                    let highlightArr=props.aTour.highlights.concat(aHighlight)
+                    props.tourEditor({
+                        ...props.aTour,
+                        "highlights": highlightArr
+                    })
+                    setAHighlight('')
+                }} />
+            </>}
+        </div>
+    )
+}
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 export function AdminLogIn(props){
-
     const [logInobj, setLogInOb]=useState()
-
     const adminLogInSubmit=async()=>{
         let strngifiedReq= JSON.stringify()
         // submit user login
@@ -66,4 +140,3 @@ export function AdminLogIn(props){
         </form>
     )
 }
-
--- pages/tourExplorer.js
+++ pages/tourExplorer.js
@@ -1,27 +1,74 @@
 import { useState, useEffect } from "react";
 
 import ExploreIcon from '@mui/icons-material/Explore';
-
+import {ItineraryImagePicker, LogoSwitcher, HighlightAdder} from "./../components/forms"
+import CancelIcon from '@mui/icons-material/Cancel';
 
 import styles from "../styles/pages/tourExplorer.module.css"
 
 export default function TourExplorerPage(props){
 
+    const [aTourCreator, setTourCreator]=useState({
+        "ecoAndesLogo": true,
+        "highlights":[]
+    })
     const tourExplorerIntro=()=>{
+        return(<>
+            <div className={styles.teIntroCont}>
+                <div className={styles.iconCont}>
+                    <ExploreIcon fontSize="large" />
+                </div>
+                <h1> Tour Explorer</h1>
+            </div>
+        </>)
+    }
+    const removeHighlight=(aList, prodIndex)=>{
+        let tempList=[...aList];
+        tempList.splice(prodIndex, 1)
+        setTourCreator({...aTourCreator,
+            "highlights": tempList
+        })
+    }
+    const highlightDispAdder=()=>{
+        let theHighlights;
+        if(aTourCreator.highlights){
+            theHighlights = aTourCreator.highlights.map((elem, i)=>
+            <div className={styles.tourHighlightCont}>
+                <div className={styles.aHighlight}> - {elem}</div>
+                <CancelIcon onClick={()=>{
+                    removeHighlight(aTourCreator.highlights, i)
+                }} />
+            </div>)
+        }
 
         return(<>
-        <div className={styles.teIntroCont}>
-            <div className={styles.iconCont}>
-                <ExploreIcon fontSize="large" />
+            <div className={styles.highlightList}> 
+            <h2> Tour Highlights: </h2>
+                {theHighlights} 
             </div>
-            <h1> Tour Explorer</h1>
-        </div>
+            <div>
+                <HighlightAdder 
+                    aTour={aTourCreator} 
+                    tourEditor={setTourCreator} 
+                />
+            </div>
         </>)
     }
 
+
     return(<>
-        <div className={styles.tourExplorerCont}>
+        <div className={styles.generalPageCont}>
             {tourExplorerIntro()}
+            <ItineraryImagePicker 
+                aTour={aTourCreator} 
+                tourEditor={setTourCreator} 
+            />
+            <LogoSwitcher 
+                aTour={aTourCreator} 
+                tourEditor={setTourCreator} 
+            />
+            {highlightDispAdder()}
+
         </div>
     </>)
 }
