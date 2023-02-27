02a8fb17165f3108f8c240f5d7b9b340c829b7a5
David Torres <djjabbua@gmail.com>
2023-02-27 01:48:26 -0500

    advances with inputs, detailing

--- pages/playground.js
+++ pages/playground.js
@@ -111,88 +111,109 @@ export default function PlaygroundPage(props){
         </>)
     }
 
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// PICKER
+// PICKER
+
+    // Sample Table
     let someOptsArr=["cucu", "rice", "some", "soda"]
+    // Sampleelem to be altered
     const [singlePickerElem, setPickerElem]=useState()
-    const [multiPickerArr, setMultiPickerArr]=useState([])
-    const [pickerOptsArray, setPickerOptsArr]=useState(someOptsArr)
-    const aDropdownPicker=(singOrMulti, theOptsArr, inputLabel, inputId, setTheResult, setMultiArr)=>{
-        if(singOrMulti==="single"){
-            let theOptions=theOptsArr.map((elem,i)=><React.Fragment key={i}> 
-                <option value={elem}> {elem} </option></React.Fragment>)
-            return(<>
-                <div className={styles.theInputContainer}>
-                    <div className={styles.anInputRow}>
-                        <label htmlFor={inputId} className={styles.inputLabel}>   
-                            {inputLabel}</label>
-                        <i> &nbsp; - &nbsp; single option picker </i>
-                    </div>
-                    <select className={styles.inputUserUI}
-                    onChange={(e)=>{
-                        e.preventDefault()
-                        setTheResult(e.target.value)
-                    }}>
-                        {theOptions}
-                    </select>
+
+    const aDropdownPicker=(theOptsArr, inputLabel, inputId, setTheResult)=>{
+        let theOptions=theOptsArr.map((elem,i)=><React.Fragment key={i}> 
+            <option value={elem}> {elem} </option></React.Fragment>)
+        return(<>
+            <div className={styles.theInputContainer}>
+                <div className={styles.anInputRow}>
+                    <label htmlFor={inputId} className={styles.inputLabel}>   
+                        {inputLabel}</label>
+                    <i> &nbsp; - &nbsp; single option picker </i>
                 </div>
-            </>)} 
-        else if(singOrMulti==="multi"){
-
-
-            // Problems with getting selector to update ui. if selected, elem will delete, but selector state turns to same index, seeming like opt is not chosen. uploading to git to have a back up and deleting this section and everything with it.
-
-
-
-
-
-            let theOptions=theOptsArr.map((elem,i)=><React.Fragment key={i}> 
-                <option value={elem} key={i}>{elem}</option></React.Fragment>)
-
-            const setResults=(e)=>{
-                e.preventDefault()
-                setTheResult([...multiPickerArr, e.target.value])
-                // temp array, splice, and remove from optArr
-                // setPickerOptsArr(spliced temp array)
-                const elemChecker = (elem) => elem === e.target.value
-                const elemIndex=someOptsArr.findIndex(elemChecker)
-                let tempList=[...pickerOptsArray];
-                tempList.splice(elemIndex, 1)
-                setPickerOptsArr(tempList)
-
-            }
-            return(<>
-                <div className={styles.theInputContainer}>
-                    <div className={styles.anInputRow}>
-                        <label htmlFor={inputId} className={styles.inputLabel}>   
-                            {inputLabel}</label>
-                        <i> &nbsp; - &nbsp; multi option picker </i>
-                    </div>
-                    <select className={styles.inputUserUI}
-                    onChange={(e)=>{
-                        setResults(e)
-                            
-
-                        
-                    }}>
-                        {theOptions}
-                    </select>
-                </div>
-            </>)}
+                <select className={styles.inputUserUI}
+                onChange={(e)=>{
+                    e.preventDefault()
+                    setTheResult(e.target.value)
+                }}>
+                    {theOptions}
+                </select>
+            </div>
+        </>) 
     }
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// MULTI OPT SELECTION
+// MULTI OPT SELECTION
+    const [multiOptElem, setMultiOptElem]=useState([])
+    
+    const multiOptPicker=(theOptsArr, inputLabel, inputId, resultingList, addToSelection, setOptsArr)=>{
+
+        let eachMultiOptDisp=theOptsArr.map((elem,i)=><React.Fragment key={i}> 
+            <div className={styles.multiPickerOpt} onClick={()=>{
+                // add to optEleem, rmv from temp arr
+                let tempList=resultingList.concat(elem)
+                addToSelection(tempList)
+                let tempListTwo = [...theOptsArr];
+                tempListTwo.splice(i,1)
+                setOptsArr(tempListTwo)
+            }} >+ {elem}</div>
+        </React.Fragment>)
+
+        let resultingMultiList=resultingList.map((elem,i)=><React.Fragment key={i}> 
+            <div className={styles.multiElemSelects} onClick={()=>{
+                // remove from selectedElems, add to OptList
+                let tempList=theOptsArr.concat(elem)
+                setOptsArr(tempList)
+                let tempListTwo = [...resultingList];
+                tempListTwo.splice(i,1)
+                addToSelection(tempListTwo)
+            }}> 
+                {elem} &nbsp; <HighlightOffIcon />
+            </div>
+        </React.Fragment>)
+        return(<>
+            <div className={styles.theInputContainer}>
+                <div className={styles.anInputRow}>
+                    <label htmlFor={inputId} className={styles.inputLabel}>   
+                        {inputLabel} picker</label>
+                    <i> &nbsp; - &nbsp; multi option picker </i>
+                </div>
+                <br/>
+                <div className={styles.aRow}>
+                    {eachMultiOptDisp}
+                </div>
+                <div className={styles.listDisplayer}>
+                    <h3> {inputLabel} </h3>
+                    <div>
+                        {resultingList.length>0?<>
+                            <div className={styles.aRow}>
+                                {resultingMultiList}
+                            </div>
+                        </>:<>
+                            <div className={styles.listPlaceholder}>...add to {inputLabel}</div>
+                        </>}
+                    </div>
+                </div>
+            </div>
+        </>)
+    }
+
+    const [tempOptArr, setTempOptArr]=useState(someOptsArr)
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// Sample Form
+// Sample Form
     const formBuilder=()=>{
         return(<>
-
             <h1> Cucu Form Builder</h1>
-
             <form> 
                 <h4> OP Single Elem Picker  </h4>
-                    {aDropdownPicker("single", someOptsArr, "Single Opt Picker", "inputIDD", setPickerElem)}
+                    {aDropdownPicker(someOptsArr, "Single Opt Picker", "inputIDD", setPickerElem)}
 
-
-
-                <h4> Multi Elem Picker </h4>
-                    {aDropdownPicker("multi", pickerOptsArray, "Multi Opt Picker", "inputID", setMultiPickerArr, setPickerOptsArr)}
-
-
+                <h4> Multi Option Picker </h4>
+                    {multiOptPicker(tempOptArr, "Destintions", "mulitInput", multiOptElem, setMultiOptElem, setTempOptArr )}
 
                 <h4> OP Text input</h4>
                     {anInputDisplayer("Tester Label", "firstInputText", "text", false, "a Text Input" )}
@@ -206,13 +227,6 @@ export default function PlaygroundPage(props){
         </>)
     }
 
-
-
-
-
-
-
-    console.log(multiPickerArr, "multi info arr")
     return(<>
         {session&&<> 
             <GMSNavii  user={session.user} signOut={<SignOutBtn/>} />
@@ -228,8 +242,6 @@ export default function PlaygroundPage(props){
         <ul> Drop downs </ul>
         <ul> Img picker </ul>
         <ul> Gen Switchers </ul>
-        
-        
 
         <br/>
         Finish tourMaker - Displayer
